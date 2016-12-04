c17152b3fc1d19b2fdb1a18ad557ffee004ef9f9
NoelBao <noeleon930@gmail.com>
2016-12-04 22:10:03 +0800

    reset

--- app/node0/app.js
+++ app/node0/app.js
@@ -14,7 +14,7 @@ app.use(cors());
 // mongoDB
 var mongo = require('mongodb');
 var monk = require('monk');
-var db = monk('127.0.0.1:27017/node1db');
+var db = monk('mongodb://aptan:aptan123@140.119.164.155:27017/node1db');
 
 var routes = require('./routes/index');
 //var users = require('./routes/users');
@@ -29,21 +29,23 @@ app.set('view engine', 'jade');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Make our db accessible to our router
-app.use(function(req,res,next){
-    req.db = db;
-    next();
+app.use(function (req, res, next) {
+  req.db = db;
+  next();
 });
 
 app.use('/', routes);
 //app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
@@ -54,7 +56,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+  app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
@@ -65,7 +67,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
--- app/node0/routes/index.js
+++ app/node0/routes/index.js
@@ -2,49 +2,55 @@ var express = require('express'); //require 是node.js裡的include
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) { //if user put url to root 
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) { //if user put url to root 
+	res.render('index', {
+		title: 'Express'
+	});
 });
 
-router.get('/helloworld', function(req,res){ // if url is 140.119.x.x/helloworld
-	res.render('helloworld',{title: 'fuck that!'});
+router.get('/helloworld', function (req, res) { // if url is 140.119.x.x/helloworld
+	res.render('helloworld', {
+		title: 'fuck that!'
+	});
 });
 
 /*GET Userlist page. */
-router.get('/userlist', function(req, res) {
-    var db = req.db;
-    var collection = db.get('usercollection');
-    collection.find({},{},function(e,docs){
-        res.render('userlist', {
-            "userlist" : docs
-        });
-    });
+router.get('/userlist', function (req, res) {
+	var db = req.db;
+	var collection = db.get('usercollection');
+	collection.find({}, {}, function (e, docs) {
+		res.json(docs.filter(d => !(d.username.includes('鮑聖文') || d.username.includes('測試'))));
+	});
 });
 
 
 /* GET New User page. */
-router.get('/newuser', function(req, res) {
-    res.render('newuser', { title: 'Add New User' });
+router.get('/newuser', function (req, res) {
+	res.render('newuser', {
+		title: 'Add New User'
+	});
 });
 
 
-router.get('/emailduplication', function(req, res) {
-    res.render('emailduplication', { title: 'emailduplication' });
+router.get('/emailduplication', function (req, res) {
+	res.render('emailduplication', {
+		title: 'emailduplication'
+	});
 });
 
 
 /* POST to Add User Service */
-router.post('/adduser', function(req, res) {
+router.post('/adduser', function (req, res) {
 
-    // Set our internal DB variable
-    var db = req.db;
+	// Set our internal DB variable
+	var db = req.db;
 
-    // Get our form values. These rely on the "name" attributes
-    var userName = req.body.username;
-    var userEmail = req.body.useremail;
+	// Get our form values. These rely on the "name" attributes
+	var userName = req.body.username;
+	var userEmail = req.body.useremail;
 
-    // Set our collection
-    var collection = db.get('usercollection');
+	// Set our collection
+	var collection = db.get('usercollection');
 
 	/*var num_of_row = collection.find({"email":userEmail}).count();    
 	if ( num_of_row != 0 ){
@@ -53,36 +59,35 @@ router.post('/adduser', function(req, res) {
 	wrong statement,don't know why
 	*/
 
-	collection.findOne({email:userEmail}, function(err, doc) 
-	{
-	    if (doc) 
-	    {
-	        res.send('duplicated email');
-	    }
-	    else
-	    {
-		    collection.insert
-		    (
-			    {
-			        "username" : userName,
-			        "email" : userEmail
-			    }, function (err, doc) 
-			    	{
-				        if (err) 
-				        {
-				            // If it failed, return error
-				            res.send("There was a problem adding the information to the database.");
-				        }
-				        else 
-				        {
-				            // And forward to success page
-				            //res.redirect("userlist");
-				            res.send('Thank you for coming!')
-				        }
-				    }
-		    );
-		}   
-		    
+	collection.findOne({
+		email: userEmail
+	}, function (err, doc) {
+		if (doc) {
+			res.status(400).json({
+				status: 'duplicated'
+			});
+		} else {
+			collection.insert({
+				"username": userName,
+				"email": userEmail
+			}, function (err, doc) {
+				if (err) {
+					// If it failed, return error
+					res.status(500).json({
+						status: false
+					});
+
+					console.log(err)
+				} else {
+					// And forward to success page
+					//res.redirect("userlist");
+					res.status(201).json({
+						status: true
+					});
+				}
+			});
+		}
+
 	});
 
 });
